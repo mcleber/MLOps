2e8e48829186f87b8f56a5a0c14865f1eb8b8c4f
Cleber <cleber_moretti@yahoo.com>
2024-12-13 23:54:31 -0300

    Add small updates

--- app/main.py
+++ app/main.py
@@ -1,82 +1,92 @@
 import os
 import mlflow
+from mlflow.exceptions import MlflowException
 import json
 import uvicorn
 import numpy as np
 from pydantic import BaseModel
 from fastapi import FastAPI
 
-"""
-    Creating an Application with FastAPI
 
-    This FastAPI application is designed to provide endpoints for checking the health status 
-    of the API and for making predictions related to fetal health. It has the following endpoints:
+class FetalHeathData(BaseModel):
+    accelerations: float
+    fetal_movement: float
+    uterine_contractions: float
+    severe_decelerations: float
 
-    1. GET /: Returns the health status of the API.
-    2. POST /predict: Makes a prediction using a model (currently returning a placeholder value).
-"""
 
-# Initialize the FastAPI application with a custom title and tags for documentation
 app = FastAPI(title="Fetal Health API",
                 openapi_tags=[ 
                     {
-                        "name": "Health",  # Tag for health-related endpoints
-                        "description": "Get API health status"  # Description of the health tag
+                        "name": "Health",  
+                        "description": "Get API health status"  
                     }, 
                     {
-                        "name": "Prediction",  # Tag for prediction-related endpoints
-                        "description": "Model prediction"  # Description of the prediction tag
+                        "name": "Prediction",  
+                        "description": "Model prediction"  
                     }
                 ])
 
 
-def loaded_model():
+def load_model():
     print("reading model...")
+
     MLFLOW_TRACKING_URI = 'https://dagshub.com/clebermoretti/mlops_cardiotocography.mlflow'
     MLFLOW_TRACKING_USERNAME = 'clebermoretti'
     MLFLOW_TRACKING_PASSWORD = 'c6b0c9924c4810d3edd0945b15c2d0a6f2125ff1'
+    
     os.environ['MLFLOW_TRACKING_USERNAME'] = MLFLOW_TRACKING_USERNAME
     os.environ['MLFLOW_TRACKING_PASSWORD'] = MLFLOW_TRACKING_PASSWORD
 
     print("setting mlflow...")
+
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-    print("creating client...")
-    client = mlflow.MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+    try:
+        print("creating client...")
+        client = mlflow.MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
 
-    print("getting registered model...")
-    registered_model = client.get_registered_model('fetal_health')
+        print("getting registered model...")
+        registered_model = client.get_registered_model('fetal_health')
 
-    print("read model...")
-    run_id = registered_model.latest_versions[-1].run_id
-    logged_model = f'runs:/{run_id}/model'
-    loaded_model = mlflow.pyfunc.load_model(logged_model)
+        if not registered_model.latest_versions:
+            raise ValueError("No versions found for the model 'fetal_health'.")
 
-    print(loaded_model)
-    return loaded_model
+        print("reading model...")
+        run_id = registered_model.latest_versions[-1].run_id
+        logged_model = f'runs:/{run_id}/model'
 
-# Define a health check endpoint to check the status of the API
+        loaded_model = mlflow.pyfunc.load_model(logged_model)
+        print("Model loaded successfully.")
+        return loaded_model
+
+    except MlflowException as e:
+        print(f"Error interacting with MLflow: {e}")
+    except ValueError as e:
+        print(f"Model version error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+
 @app.get(path='/',
             tags=['Health'])
 def api_health():
-    """
-    Health check endpoint to verify if the API is up and running.
-    
-    Returns:
-        dict: A JSON response indicating the health status of the API.
-    """
-    return {"status": "healthy"}  # Returns a JSON response indicating the API is healthy
+    return {"status": "healthy"}
 
-# Define a prediction endpoint to make predictions using a model
+
 @app.post(path='/predict',
             tags=['Prediction'])
-def predict():
-    """
-    Prediction endpoint to simulate making a model prediction.
-    
-    This is a placeholder function and currently returns a fixed prediction value (0).
-    
-    Returns:
-        dict: A JSON response containing the prediction value.
-    """
-    return {"prediction": 0}  # Returns a prediction (in this case, just a placeholder value of 0)
+def predict(request: FetalHeathData):
+    loaded_model = load_model()
+
+    received_data = np.array([
+        request.accelerations,
+        request.fetal_movement,
+        request.uterine_contractions,
+        request.severe_decelerations,
+    ]).reshape(1, -1)
+
+    print(received_data)
+    print(loaded_model.predict(received_data))
+
+    return {"prediction": 0}
